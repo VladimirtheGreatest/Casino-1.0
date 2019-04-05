7cab829f3686e010f11c34980a44b25ab2cec2de
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-04-05 23:45:31 +0100

    Update casinojs.js
    Added special event feature, "free spins". Once triggered player will activate extra spin free of charge with additional chance to win extra multiplier. I will add some animation and additional design tomorrow. The core concept of the game of chance is pretty much done. I might have to test it and change the odds of winning or losing. Spin button will be disabled while active. Jackpot will increase over time while playing the game.

--- casinojs.js
+++ casinojs.js
@@ -14,7 +14,11 @@ var bigwin = range(28000, 30000); // 10 bet multiplier  2% chance to win
 
 var extraspin = range(40000, 42000); // extra 20 spins, this will be a special feature, 20 auto spins with the special random multiplicator
 
-var bankroll = "0";
+var specialwin = range(50000, 55000); // free spins range which will trigger special win during special event "free spins"
+
+var bankroll = "0"; //initial bankroll
+
+var jackpotmoney = 50000; //this will be initial jacpot incrementing with each and every single spin, after hitting a jackpot it will reset itself to 50000.
 
 
 
@@ -37,8 +41,10 @@ function randomnumber() {
       if (new Date().getTime() - startTime > 5000) { //if no win subtract 1 from bankroll if min small bet clicked, subtract 2 if big bet clicked, in the future implement auto-spin feature as well
         clearInterval(interval);
         if (bankroll != "0") {
-          bankroll -= 1
-          document.getElementById("bankroll").innerHTML = bankroll;
+          bankroll -= 1;
+          jackpotmoney += 1;
+          document.getElementById("bankroll").innerHTML = 'bankroll' + ' ' + bankroll;
+          document.getElementById("jackpot").innerHTML = ' Current jackpot is' + ' ' + jackpotmoney;
         } else {
           alert("You have to make a deposit to play again") //if bankroll falls to zero player will be encouraged to deposit again to keep playing
         }
@@ -46,24 +52,26 @@ function randomnumber() {
       }
       var result = Math.floor(Math.random() * 100000); //function that generates random number
       document.getElementById("number").innerHTML = result;
+      document.getElementById("number").className = 'animated bounceInDown';
       if (result === jackpot) { //chance to win a jackpot are 1 to 100000 still better than some pokerstars slots, good luck !:)
         clearInterval(interval);
         alert("jackpot");
-        bankroll += 50000
+        bankroll += 50000;
       } else if (extraspin.includes(result)) {
         document.getElementById("special").innerHTML = "Your extra spin! Click here!"; // button will not be displayed, once player activate the special button, regular spin will dissappear and random number function will be replaced with the special event
+        document.getElementById("special").style.visibility = 'visible';
         clearInterval(interval);
       } else if (bigwin.includes(result)) {
         document.getElementById("info").innerHTML = "BIG WIN!!!!!";
         if (bankroll != "0") {
-          bankroll += 10
+          bankroll += 10;
           document.getElementById("bankroll").innerHTML = bankroll;
         }
         clearInterval(interval);
       } else if (smallwin.includes(result)) {
         document.getElementById("info").innerHTML = "WIN!!!!!";
         if (bankroll != "0") {
-          bankroll += 2
+          bankroll += 2;
           document.getElementById("bankroll").innerHTML = bankroll;
         }
         clearInterval(interval);
@@ -71,6 +79,7 @@ function randomnumber() {
 
     }, 1000);
   }
+  setTimeout('$("#button").removeAttr("disabled")', 5000);
 }
 
 
@@ -78,10 +87,52 @@ function randomnumber() {
 
 $(document).ready(function() {
   $('#button').click(function() {
+    $(this).attr("disabled", "disabled");
     randomnumber();
-    document.getElementById("info").innerHTML = ""; ////will add some spin animation
   });
 });
+
+
+//jquery function to activate special event "free spins"
+
+$(document).ready(function() {
+  $('#special').click(function() {
+    $(this).attr("disabled", "disabled");
+    specialevent();
+    document.getElementById("special").style.visibility = 'hidden';
+    //some animation
+  });
+});
+
+//random range function to calculate multiplier in the following "free spins" function
+function randomrange() {
+  return Math.floor(Math.random() * 6) + 1;
+
+};
+
+//special "free spins" event function
+
+function specialevent() {
+  var startTime = new Date().getTime();
+  var interval = setInterval(function() {
+    if (new Date().getTime() - startTime > 15000) { 
+      clearInterval(interval);
+      return;
+    }
+    var result = Math.floor(Math.random() * 100000); //function that generates random number
+    document.getElementById("number").innerHTML = result;
+    var multiplier = randomrange();
+    if (specialwin.includes(result)) {
+      bankroll += 5 * multiplier;
+      document.getElementById("info").innerHTML = "BIG WIN";
+      document.getElementById("bankroll").innerHTML = bankroll;
+      clearInterval(interval);
+    };
+
+  }, 1000);
+  setTimeout('$("#special").removeAttr("disabled")', 15000);
+};
+
 
 
 /*jquery to activate deposit function once button bet clicked*/
@@ -100,3 +151,21 @@ function deposit() {
   bankroll = deposit + bankroll;
   document.getElementById("bankroll").innerHTML = bankroll;
 }
+
+
+// animate css function
+
+function animateCSS(element, animationName, callback) {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
+
